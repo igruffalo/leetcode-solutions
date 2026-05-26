1b86e9d2981c4eaf0a8673bae09e9db29cb93c21
igruffalo <154533813+igruffalo@users.noreply.github.com>
2026-05-26 10:32:58 -0500

    Time: 4 ms (58.47%) | Memory: 46.5 MB (63.30%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,17 +1,16 @@
 class Solution {
     public int romanToInt(String s) {
-        Map<Character,Integer> map = Map.of('I',1, 'V', 5, 'X', 10, 'L', 50, 'C', 100, 'D', 500, 'M', 1000);
+        int previous = 0;
+        int current = 0;
+         int result =0;
+         Map<Character, Integer> map = Map.of('I', 1, 'V', 5, 'X', 10, 'L', 50, 'C', 100, 'D', 500, 'M', 1000);
 
-        int result = 0;
-        for(int i =0; i< s.length(); i++) {
-            int curr = map.get(s.charAt(i));
-            int next = (i+1 < s.length()) ? map.get(s.charAt(i+1)) : 0;
-            if(curr < next ){
-                result -= curr;
-            } else {
-                result += curr;
-            }
-        }
-        return result;
+         for(int i = s.length()-1; i>=0; i--) {
+            current = map.get(s.charAt(i));
+            if(current < previous) result -= current;
+            else result += current;
+            previous = current;
+         }
+         return result;
     }
 }
